202b2107ea3febd5049c37895770c92a33f6d0b4
xwp <xwp_fullstack@163.com>
2019-02-20 15:18:41 +0800

    房屋详情页面的实现

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -4,6 +4,7 @@ from info.utils.commons import login_required
 from info.utils.response_code import RET
 from info.models import House
 
+
 @home_blue.route('/')
 def index():
     return redirect('/index.html')
@@ -40,3 +41,40 @@ def my_houses_list():
         data.append(house.to_basic_dict())
 # 返回结果
     return jsonify(errno=RET.OK, errmsg='OK', data=data)
+
+
+
+@home_blue.route('/houses/<int:house_id>', methods=["GET"])
+@login_required
+def detail_house(house_id):
+    """
+    房屋详情
+    1,建立视图函数，需要前端给后端返回房屋的ID
+    2，从g对象中获取用户信息，如果获取不到用户对象则返回user_id为-1
+    3，根据传入的hoser_id在数据库中查询house信息
+    4，house对象调用模板函数，将数据库查询到的信息保存到字典中
+    5，将信息返回给前段页面
+
+    """
+    user = g.user
+    if not user:
+        return user['user_id'] == -1
+    else:
+        user_id = user.id
+
+    try:
+        house = House.query.get(house_id)
+    except Exception as e:
+        current_app.logger.errro(e)
+        return jsonify(errno = RET.DBERR, msg = '查询房屋信息失败')
+
+
+    data = {
+        'user_id':user_id,
+        'house_data':house.to_full_dict()
+    }
+
+    return jsonify(errno = RET.OK, errmsg = 'OK' , data = data)
+
+
+
